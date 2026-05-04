7d4d53f780b4620f4f81e7d79160e3f1e6478197
Modou fall <serignemodoufall1@gmail.com>
2026-05-04 13:48:08 +0200

    update

---
+++ prototipo-parte2/datos.del.examen.md
@@ -0,0 +1,12 @@
+
+El objetivo de la segunda parte es añadir una arquitectura en capas a la API y persistencia en una DB.
+
+Utiliza un patrón de diseño MVC para la estructura del proyecto.
+Añade una capa de servicios que se encargue de la lógica de negocio.
+Añade una capa de repositorios que se encargue de la persistencia en una base de datos.
+Después de añadir la arquitectura en capas, añade una base de datos SQLite a la API. Utiliza PRISMA para la conexión con la base de datos. Puedes utilizar Zod para la validación de los datos.
+
+Criterios de evaluación (2: 2 puntos)
+Estructura del código: 1 punto.
+Acceso a la base de datos: 0.5 puntos.
+Funcionamiento correcto de la API: 0.5 puntos.
---
+++ prototipo-parte2/src/mockproducto.ts
@@ -0,0 +1,24 @@
+// Es el archivo que usare para simular mi base de datos como el exmen pide
+
+// mockProducts.ts
+export const mockProducts = [
+    // id 1
+    {
+        id: '1', // id unico del producto
+        name: 'Ordenador', // nombre del producto
+        price: 1000, // El precio del producto
+        is_active: true, // estado del producto
+        created_at: new Date(), // esta para la fecha de creacion
+        update_at: new Date(), // y este para la fecha de actualizacion
+    },
+    // id 2
+    {
+        id: '2',
+        name: 'Smartphone',
+        price: 800,
+        stock: 20,
+        is_active: true,
+        created_at: new Date(),
+        updated_at: new Date(),
+    },
+];
